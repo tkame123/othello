2e3f70f29022c002c0bb58917802a7044834518c
Takeshi Kameyama <tkame123@gmail.com>
2019-09-06 17:33:57 +0900

    fixed

--- src/presentation/component/play_room/play_room_controller.tsx
+++ src/presentation/component/play_room/play_room_controller.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from "styled-components";
 import Button from "../common/button";
 
-import {config} from "../../../util/config";
 import {PlayRoom} from "../../../domain/model/play_room";
 
 interface IProps {
@@ -13,13 +12,11 @@ interface IProps {
 const PlayRoomControllerComponent: React.FC<IProps> = (props) => {
 
     const {playRoom, handleCreateNewGame} = props;
-    const gameId: string | null = playRoom.gameId;
-
     return (
         <Wrapper>
 
             <Inner>
-                {!gameId && <Button onClick={handleCreateNewGame}>Create Game</Button>}
+                {!playRoom.gameId && <Button onClick={handleCreateNewGame}>Create Game</Button>}
             </Inner>
 
         </Wrapper>
@@ -44,23 +41,4 @@ const Inner = styled.div`
     width: 100%;
     padding-left: 10px;
     padding-right: 10px;
-`;
-
-const DeleteButton = styled(Button)`
-    padding: 0.5em;
-    border-Radius: 3px;
-    border-style: none;
-    background-color: ${config().style.color.error};
-    text-Align: center;
-    color: ${config().style.color.onError};
-    cursor: pointer;
-    &:hover {
-      background: ${config().style.color.error};
-    }
-    &:disabled {
-      background-color: ${config().style.color.error};
-      color: rgba(255, 255, 255, 0.5);
-      &:hover {
-        background-color: ${config().style.color.error};
-      }
 `;
